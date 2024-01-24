25339aff045584cfd3ae473e6bc68c08b84a8c87
vk1408 <84286885+vk1408@users.noreply.github.com>
2024-01-24 22:43:29 +0100

    Add project files.

---
+++ SerializeToJSON/Group.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializeToJSON
+{
+    public class Group
+    {
+        public string? Name { get; set; }
+        public int DeviceCount { get; set; }
+    }
+}
---
+++ SerializeToJSON/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+namespace SerializeToJSON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // initialize list
+            List<Group> groups = new List<Group>() 
+            { 
+                new Group() {Name="Storage", DeviceCount=3},
+                new Group() {Name="Production", DeviceCount=15},
+                new Group() {Name="Depot", DeviceCount=23}
+            };
+
+            var jstring = JsonSerializer.Serialize(groups);
+            
+            string folder = Directory.GetCurrentDirectory();
+            string fileName = "groups.json";
+
+            File.WriteAllText(folder +"\\"+fileName, jstring);
+            Console.WriteLine("Write to file done!");
+
+            var data = File.ReadAllText(folder +"\\"+fileName);
+
+            List<Group> groupData = new List<Group>();
+            groupData = JsonSerializer.Deserialize<List<Group>>(data);
+
+            Console.WriteLine("Read from file done!");
+
+        }
+    }
+}
